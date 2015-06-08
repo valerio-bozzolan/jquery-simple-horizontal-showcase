0fd36430b211e66e5ae3b2ea5e1760e4a6b6bf66
Valerio Bozzolan <valerio.bozzolan@gmail.com>
2015-06-09 01:09:39 +0200

    Examples

--- README.md
+++ README.md
@@ -1,66 +1,21 @@
 jQuery plugin for a simple horizontal showcase
 ==============================================
+This no-frills horizontal-showcase want to be styled as YOU want (not as I want). So it has only 3-4 CSS rules.
+
+The idea is to work without clown clothes.
 
 ### Licence
-This jQuery plugin is licensed under the GNU Affero General Public License.
-Please, read carefully: I've wrote «AFFERO». Read carefully it.
+This jQuery plugin is licensed under the GNU Affero General Public License (yes.. «AFFERO»).
  * [GNU AGPL license](http://www.gnu.org/licenses/agpl-3.0.html) on gnu.org
  * [GNU AGPL Explained](http://www.gnu.org/licenses/why-affero-gpl.en.html) on gnu.org
 
 ### Usage
-It's really simple.
-```html
-<!-- Importing jQuery -->
-<!-- Importing this horizontal-showcase.js -->
-<!-- Importing 8 CSS rules -->
+See the local ```examples``` folder or see it online:
 
-<div class="showcase">
-	<button class="show-back float-left">Back</button>
-	<div class="show-window">
-		<div class="show-container">
-			<div class="show-el">Hello, world!</div>
-			<div class="show-el">GNU/Linux</div>
-			<div class="show-el">GNU/Hurd</div>
-			<div class="show-el">Debian/Linux</div>
-			<div class="show-el">Debian/Hurd</div>
-		</div>
-	</div>
-	<button class="show-forward float-left">Forward</button>
-</div>
-
-<script>
-$(document).ready(function() {
-	$(".showcase").horizontalShowcase();
-});
-</script>
-```
-
-Lot of options as (they are not all):
-```javascript
-$(".showcase").horizontalShowcase({
-	preventFlood: true, /* prevent click during existing animation */
-	click: function(orizhontalShowcase) { /* fired when clicking on a button */
-	    // $(this) is the button
-	},
-	done: function(orizhontalShowcase) { /* fired when animation ends */
-	    // $(this) is the button
-	},
-	elSize: 400, /* element width */
-	elSpaceLeft: 0, /* manually insert this sum: border-left + padding-left */
-	elSpaceRight: 0, /* manually insert this sum: border-right + padding-right */
-	elSpace: undefined, /* manually insert this sum: border-left + padding-left + border-right + padding-right (this override the above options) */
-	delay: 200, /* scroll animation */
-	overflow: true, /* enable turn around */
-	overflowDelay: 500, /* turn around scroll animation */
-	step: undefined, /* how many elements to scroll (ca be float) */
-	interval: undefined, /* set an interval in ms */
-	windowSize: undefined, /* window size specified in px */
-	windowEls: 1 /* window size specified in n° of elements (this override the above option) */
-});
-```
+http://horizontal-showcase.reyboz.it/examples/
 
 ### The CSS
-Really, see the file. There are 8 CSS rules.
+Really, see the file. There are only 3-4 CSS rules.
 
-### Credits.
+### Credits
 Thanks to http://jsfiddle.net/JQZ29/24/ for little clarifications.
--- horizontal-showcase.js
+++ horizontal-showcase.js
@@ -19,111 +19,105 @@
 	$.fn.horizontalShowcase = function( options ) {
 		var opts = $.extend( {}, $.fn.horizontalShowcase.defaults, options );
 
-		var $showcase = this;
+		return this.each(function() {
+			var $showcase = $(this);
 
-		// Specified element's width
-		var $els = this.find(opts.elSelector);
-		$els.width(opts.elWidth);
-		opts.elWidth = opts.elWidth + opts.elInterWidth; // We formally think that the element is even with padding and border
-		if(!opts.elHeight) {
-			var maxVal = -1;
-			$els.each(function() {
-				var h = $(this).height(); 
-				maxVal = h > maxVal ? h : maxVal;
-				console.log("Heiht: " + h);
-			});
-			opts.elHeight = maxVal;
-		}
-		$els.height(opts.elHeight);
-
-		opts.step = opts.step || opts.els;
-
-		// Specified window's width
-		opts.width = opts.width || opts.els * opts.elWidth;
-		this.find(opts.windowSelector).width(opts.width).height(opts.elHeight + opts.elInterHeight); // We formally think that the window is even with padding and borders of the elements
-
-		// Calculated container's width
-		var $container = this.find(opts.containerSelector);
-		var containerWidth = $els.length * opts.elWidth;
-		$container.width(containerWidth);
-
-		// Calculated scroll limit (when you can see the last part of the container and it can turn around)
-		var slideLimit = opts.width - containerWidth;
-
-		var $btnBack = this.find(opts.actionBackSelector);
-		$btnBack.click(function(event) {
-			restartInterval();
-
-			var x = $container.position();
-			var y = x.left;
-			var yy = y + opts.elWidth * opts.step;
-
-			if(opts.preventDefault) {
-				event.preventDefault();
+			// Specified element's width
+			var $els = $(this).find(opts.elSelector);
+			$els.width(opts.elWidth);
+			opts.elWidth = opts.elWidth + opts.elInterWidth; // We formally think that the element is even with padding and border
+			if(!opts.elHeight) {
+				var maxVal = -1;
+				$els.each(function() {
+					var h = $(this).height(); 
+					maxVal = h > maxVal ? h : maxVal;
+				});
+				opts.elHeight = maxVal;
 			}
+			$els.height(opts.elHeight);
 
-			if(y >= 0 || opts.__animating === true) {
-				return;
-			}
+			opts.step = opts.step || opts.els;
 
-			opts.__animating = true;
-			opts.click.call(this, $showcase);
-			$container.animate({left: yy}, opts.delay, function() {
-				opts.__animating = false;
-				opts.done.call($btnBack, $showcase);
-			});
-		});
+			// Specified window's width
+			opts.width = opts.width || opts.els * opts.elWidth;
+			$(this).find(opts.windowSelector).width(opts.width).height(opts.elHeight + opts.elInterHeight); // We formally think that the window is even with padding and borders of the elements
 
-		var $btnForward = this.find(opts.actionForwardSelector);
-		var restartInterval = function() {
-			if(opts.interval) {
-				if(opts.__intervalID) {
-					clearInterval(opts.__intervalID);
+			// Calculated container's width
+			var $container = $(this).find(opts.containerSelector);
+			var containerWidth = $els.length * opts.elWidth;
+			$container.width(containerWidth);
+
+			// Calculated scroll limit (when you can see the last part of the container and it can turn around)
+			var slideLimit = opts.width - containerWidth;
+
+			var $btnBack = $(this).find(opts.actionBackSelector);
+			$btnBack.click(function(event) {
+				restartInterval();
+
+				var x = $container.position();
+				var y = x.left;
+				var yy = y + opts.elWidth * opts.step;
+
+				if(opts.preventDefault) {
+					event.preventDefault();
 				}
-				opts.__intervalID = setInterval(function() {
-					$btnForward.click();
-				}, opts.interval);
-			}
-		};
-		$btnForward.click(function(event) {
+
+				if(y >= 0 || opts.__animating === true) {
+					return;
+				}
+
+				opts.__animating = true;
+				opts.click.call($(this), $showcase);
+				$container.animate({left: yy}, opts.delay, function() {
+					opts.__animating = false;
+					opts.done.call($btnBack, $showcase);
+				});
+			});
+
+			var $btnForward = $(this).find(opts.actionForwardSelector);
+			var restartInterval = function() {
+				if(opts.interval) {
+					if(opts.__intervalID) {
+						clearInterval(opts.__intervalID);
+					}
+					opts.__intervalID = setInterval(function() {
+						$btnForward.click();
+					}, opts.interval);
+				}
+			};
+			$btnForward.click(function(event) {
+				restartInterval();
+
+				var x = $container.position();
+				var y = x.left;
+				var yy = y - opts.elWidth * opts.step;
+				var turnAround = y <= slideLimit;
+				var value = (turnAround) ? 0 : yy;
+
+				if(opts.preventDefault) {
+					event.preventDefault();
+				}
+
+				if(turnAround && opts.oveflow === false || opts.__animating === true) {
+					return;
+				}
+
+				opts.__animating = true;
+				opts.click.call($(this), $showcase);
+				$container.animate({left: value}, (turnAround) ? opts.overflowDelay : opts.delay, function() {
+					opts.__animating = false;
+					opts.done.call($btnForward, $showcase);
+				});
+			});
+
 			restartInterval();
 
-			var x = $container.position();
-			var y = x.left;
-			var yy = y - opts.elWidth * opts.step;
-			var turnAround = y <= slideLimit;
-			var value = (turnAround) ? 0 : yy;
-
-			if(opts.preventDefault) {
-				event.preventDefault();
-			}
-
-			if(turnAround && opts.oveflow === false || opts.__animating === true) {
-				return;
-			}
-
-			opts.__animating = true;
-			opts.click.call(this, $showcase);
-			$container.animate({left: value}, (turnAround) ? opts.overflowDelay : opts.delay, function() {
-				opts.__animating = false;
-				opts.done.call($btnForward, $showcase);
-			});
 		});
-
-		restartInterval();
-
-		return this;
 	};
 	$.fn.horizontalShowcase.defaults = {
-		windowSelector: ".show-window",
-		containerSelector: ".show-container",
-		elSelector: ".show-el",
-		actionBackSelector: ".show-back",
-		actionForwardSelector: ".show-forward",
-		preventDefault: false,
-		preventFlood: true,
-		click: function(orizhontalShowcase) {},
-		done: function(orizhontalShowcase) {},
+		els: 1,
+		step: undefined,
+		width: undefined,
 		elWidth: 250,
 		elHeight: undefined,
 		elInterWidth: 0,
@@ -131,9 +125,15 @@
 		delay: 200,
 		overflow: true,
 		overflowDelay: 500,
-		step: undefined,
 		interval: undefined,
-		width: undefined,
-		els: 1
+		click: function(orizhontalShowcase) {},
+		done: function(orizhontalShowcase) {},
+		preventDefault: false,
+		preventFlood: true,
+		windowSelector: ".show-window",
+		containerSelector: ".show-container",
+		elSelector: ".show-el",
+		actionBackSelector: ".show-back",
+		actionForwardSelector: ".show-forward"
 	};
 }( jQuery ));
